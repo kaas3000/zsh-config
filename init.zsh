c906304b4c5ae6710d93a7d9dcced32ad6929aec
Kees Meliefste <k_meliefste@hotmail.nl>
2017-11-09 19:18:35 +0100

    New plugin, case insensitive completion

--- init.zsh
+++ init.zsh
@@ -7,6 +7,9 @@ unsetopt share_history
 # Init zplug
 source ~/.zplug/init.zsh
 
+# Case insensitive matches for autocompletion
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 # Oh my zsh git plugin providing git aliases
 zplug "plugins/git", from:oh-my-zsh
 
@@ -18,6 +21,9 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Fish autocomplete for ZSH
 zplug "zsh-users/zsh-autosuggestions"
+
+# Fish substring history search for ZSH
+zplug "zsh-users/zsh-history-substring-search"
 
 zplug load
 
