3be1245e4578d791097b8e65a7eeb2771b1ba112
Kees Meliefste <k_meliefste@hotmail.nl>
2017-12-11 08:26:10 +0100

    Added history settings

--- init.zsh
+++ init.zsh
@@ -3,6 +3,13 @@
 # Add leinigen zsh completions (clojure package manager)
 source $KAAS3000ZSHROOT/completions/lein_completion.zsh
 
+# History settings
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt appendhistory
+
+
 # Init zplug
 source ~/.zplug/init.zsh
 
